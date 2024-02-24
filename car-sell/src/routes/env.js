532f07fa99fc9e38d2957ab274d227fac244af77
Shobhit Gupta <guptashobhit2024@gmail.com>
2024-02-24 20:14:12 +0530

    sir

--- car-sell/src/routes/env.js
+++ car-sell/src/routes/env.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://car-sell-red.vercel.app";
+export const BACKEND_URL = "https://car-sell-six.vercel.app";
 // "https://car-sell-red.vercel.app";
 // "http://localhost:3000";
